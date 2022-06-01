380738fa94cf39f8024a2f58b959e300bee32b98
Leno4ek86 <102864231+Leno4ek86@users.noreply.github.com>
2022-06-01 14:28:07 +0300

    добавлен код анкеты

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле паспортных данных
+    public String patronymic; // поле отчества
+    public String phone; // поле телефонного номера
+    public String surname; // поле фамилия
+    public boolean subscription;
+    public FormDate birthday;
+
+}
+
+
+
+
+
